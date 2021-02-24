5271e3ace7a7932a0f3dfb4f4cf33a0c26cbeddd
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2021-02-24 16:54:35 +0700

    Rewrite helper functions in tree-sitter-langs-build

--- tree-sitter-langs-build.el
+++ tree-sitter-langs-build.el
@@ -48,7 +48,7 @@
 
 (defconst tree-sitter-langs--bundle-version "0.9.2"
   "Version of the grammar bundle.
-This is bumped whenever `tree-sitter-langs-repos' is updated, which should be
+This should be bumped whenever a language submodule is updated, which should be
 infrequent (grammar-only changes). It is different from the version of
 `tree-sitter-langs', which can change frequently (when queries change).")
 
@@ -78,56 +78,49 @@ If VERSION and OS are not specified, use the defaults of
           version
           (tree-sitter-langs--bundle-file ".gz" version os)))
 
-;;; A list of (LANG-SYMBOL VERSION-TO-BUILD &optional PATHS REPO-URL).
-(defconst tree-sitter-langs-repos
-  '((agda       "d710ff1")
-    (bash       "f226a4b")
-    (c          "80b6b74")
-    (c-sharp    "aae8ab2")
-    (cpp        "5e7476b")
-    (css        "23f2cb9")
-    (elm        "06a8ff7" nil "https://github.com/razzeee/tree-sitter-elm")
-    (fluent     "858fdd6")
-    (go         "f5cae4e")
-    (html       "92c17db")
-    (janet-simple "45418f7" nil "https://codeberg.org/sogaiu/tree-sitter-janet-simple")
-    (java       "0b18a22")
-    (javascript "6296f90")
-    (jsdoc      "77e7785")
-    (json       "d3976b2")
-    (julia      "165e2ae")
-    (ocaml      "9e4f226")
-    (php        "7df0460")
-    (python     "649e752")
-    (ruby       "1ce58f2")
-    (rust       "40620bf")
-    (scala      "904e2b1")
-    (swift      "a22fa5e")
-    (typescript "a3a4bec" ("typescript" "tsx")))
-  "List of language symbols and their corresponding grammar sources.
-Note that these are mostly for the grammars. We treat the queries they include
-as references, instead of using them directly for syntax highlighting.
-
-If the grammar is not in an \"official\" repo (i.e. belonging to the
-organization https://github.com/tree-sitter), the repo URL must be specified.
-For example:
-    (hit \"bdeac01\" nil \"https://github.com/dschwen/tree-sitter-hit\")")
-
 (defconst tree-sitter-langs--repos-dir
   (file-name-as-directory
    (concat tree-sitter-langs--dir "repos"))
   "Directory to store grammar repos, for compilation.")
 
-(defun tree-sitter-langs--source (lang-symbol)
-  "Return a plist describing the source of the grammar for LANG-SYMBOL."
-  (when-let ((source (alist-get lang-symbol tree-sitter-langs-repos)))
-    (let ((version (or (nth 0 source) "origin/master"))
-          (paths (or (nth 1 source) (list "")))
-          (repo (or (nth 2 source) (format "https://github.com/tree-sitter/tree-sitter-%s"
-                                           (symbol-name lang-symbol)))))
-      (list :repo repo :version version :paths paths))))
+(defun tree-sitter-langs--symbol (name)
+  "Return the languae symbol from the NAME of the repository."
+  (intern (replace-regexp-in-string "tree-sitter-" "" name)))
+
+(defun tree-sitter-langs--repo-name (lang-symbol)
+  "Return the language repository name of LANG-SYMBOL."
+  (format "tree-sitter-%s" lang-symbol))
 
 (defvar tree-sitter-langs--out nil)
+
+(defmacro tree-sitter-langs--with-temp-buffer (&rest body)
+  "Execute BODY with `tree-sitter-langs--out' bound to the temporary buffer."
+  (declare (indent 0))
+  `(with-temp-buffer
+     (let* ((tree-sitter-langs--out (current-buffer)))
+       ,@body)))
+
+(defun tree-sitter-langs--source (lang-symbol)
+  "Return a plist describing the source of the grammar for LANG-SYMBOL."
+  (let* ((name (tree-sitter-langs--repo-name lang-symbol))
+         (dir (concat tree-sitter-langs--repos-dir name))
+         (sub-path (format "repos/%s" name)))
+    (when (file-directory-p dir)
+      (list
+       :repo (tree-sitter-langs--with-temp-buffer
+               (let ((default-directory tree-sitter-langs--dir))
+                 (tree-sitter-langs--call
+                  "git" "config" "--file" ".gitmodules"
+                  "--get" (format "submodule.%s.url" sub-path)))
+               (goto-char 1)
+               (buffer-substring-no-properties 1 (line-end-position)))
+       :version (tree-sitter-langs--with-temp-buffer
+                  (tree-sitter-langs--call
+                   "git" "submodule" "status" "--cached" sub-path)
+                  (buffer-substring-no-properties 2 9))
+       :paths (pcase lang-symbol
+                ('typescript '("typescript" "tsx"))
+                (_ '("")))))))
 
 ;;; TODO: Use (maybe make) an async library, with a proper event loop, instead
 ;;; of busy-waiting.
@@ -153,32 +146,35 @@ If BUFFER is nil, `princ' is used to forward its stdout+stderr."
     (unless (= exit-code 0)
       (error "Error calling %s, exit code is %s" command exit-code))))
 
+(defun tree-sitter-langs--map-repos (fn)
+  "Call FN in each of the language repositories."
+  (thread-last (directory-files tree-sitter-langs--repos-dir)
+    (seq-map (lambda (name)
+               (let ((dir (concat tree-sitter-langs--repos-dir name)))
+                 (when (and (string-prefix-p "tree-sitter-" name)
+                            (file-directory-p dir))
+                   `(,name . ,dir)))))
+    (seq-filter #'identity)
+    (seq-map (lambda (d)
+               (pcase-let ((`(,name . ,default-directory) d))
+                 (funcall fn name))))))
+
 (defun tree-sitter-langs--update-repos ()
   "Update lang repos' remotes."
-  (seq-map
-   (lambda (desc)
-     (pcase-let* ((`(,lang-symbol . _) desc)
-                  (default-directory (concat tree-sitter-langs--repos-dir
-                                             (format "tree-sitter-%s" lang-symbol))))
-       (tree-sitter-langs--call "git" "remote" "update")))
-   tree-sitter-langs-repos))
+  (tree-sitter-langs--map-repos
+   (lambda (_) (tree-sitter-langs--call "pwd"))))
 
 (defun tree-sitter-langs--get-latest (type)
-  "Return the `tree-sitter-langs-repos' with versions replaced by latest tags/commits.
+  "Return the latest tags/commits of the language repositories.
 TYPE should be either `:commits' or `:tags'. If there's no tag, return the
 latest commit."
   (require 'magit)
-  (seq-map
-   (lambda (desc)
-     (pcase-let* ((`(,lang-symbol _ . ,extra) desc)
-                  (default-directory (concat tree-sitter-langs--repos-dir
-                                             (format "tree-sitter-%s" lang-symbol))))
-       `(,lang-symbol ,(pcase type
-                         (:commits (magit-rev-parse "--short=7" "origin/master"))
-                         (:tags (or (magit-get-current-tag "origin/master")
-                                    (magit-rev-parse "--short=7" "origin/master"))))
-                      . ,extra)))
-   tree-sitter-langs-repos))
+  (tree-sitter-langs--map-repos
+   (lambda (name)
+     `(,name ,(pcase type
+                (:commits (magit-rev-parse "--short=7" "origin/master"))
+                (:tags (or (magit-get-current-tag "origin/master")
+                           (magit-rev-parse "--short=7" "origin/master"))))))))
 
 (defun tree-sitter-langs--buffer (name)
   "Return a buffer from NAME, as the DESTINATION of `call-process'.
@@ -194,26 +190,25 @@ In batch mode, return nil, so that stdout is used instead."
   "Download and compile the grammar for LANG-SYMBOL.
 This function requires git and tree-sitter CLI.
 
-If the optional arg CLEAN is non nil, compile from the revision specified in
-`tree-sitter-langs-repos', and clean up afterwards. Otherwise, compile from the
-current state of the grammar repo, without cleanup."
+If the optional arg CLEAN is non-nil, compile from the revision recorded in this
+project (through git submodules), and clean up afterwards. Otherwise, compile
+from the current state of the grammar repo, without cleanup."
   (message "[tree-sitter-langs] Processing %s" lang-symbol)
   (unless (executable-find "git")
     (error "Could not find git (needed to download grammars)"))
   (unless (executable-find "tree-sitter")
     (error "Could not find tree-sitter executable (needed to compile grammars)"))
   (let* ((source (tree-sitter-langs--source lang-symbol))
-         (lang-name (symbol-name lang-symbol))
          (dir (if source
                   (file-name-as-directory
                    (concat tree-sitter-langs--repos-dir
-                           (format "tree-sitter-%s" lang-name)))
-                (error "Unknown language `%s'" lang-name)))
+                           (tree-sitter-langs--repo-name lang-symbol)))
+                (error "Unknown language `%s'" lang-symbol)))
          (repo (plist-get source :repo))
          (paths (plist-get source :paths))
          (version (plist-get source :version))
          (tree-sitter-langs--out (tree-sitter-langs--buffer
-                                  (format "*tree-sitter-langs-compile %s*" lang-name))))
+                                  (format "*tree-sitter-langs-compile %s*" lang-symbol))))
     (if (file-directory-p dir)
         (let ((default-directory dir))
           (tree-sitter-langs--call "git" "remote" "-v" "update"))
@@ -263,22 +258,20 @@ current state of the grammar repo, without cleanup."
 
 (defun tree-sitter-langs-create-bundle (&optional clean)
   "Create a bundle of language grammars.
-The bundle includes all languages declared in `tree-sitter-langs-repos'.
+The bundle includes all languages under `tree-sitter-langs--repos-dir'.
 
-If the optional arg CLEAN is non nil, compile from the revisions specified in
-`tree-sitter-langs-repos', and clean up afterwards. Otherwise, compile from the
-current state of the grammar repos, without cleanup."
+If the optional arg CLEAN is non-nil, compile from the revisions recorded in
+this project (through git submodules), and clean up afterwards. Otherwise,
+compile from the current state of the grammar repos, without cleanup."
   (unless (executable-find "tar")
     (error "Could not find tar executable (needed to bundle compiled grammars)"))
-  (let ((errors (thread-last tree-sitter-langs-repos
-                  (seq-map
-                   (lambda (desc)
-                     (pcase-let ((`(,lang-symbol . _) desc))
-                       (message "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-                       (condition-case err
-                           (tree-sitter-langs-compile lang-symbol clean)
-                         (error `[,lang-symbol ,err])))))
-                  (seq-filter #'identity))))
+  (let ((errors (tree-sitter-langs--map-repos
+                 (lambda (name)
+                   (message "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                   (let ((lang-symbol (tree-sitter-langs--symbol name)))
+                     (condition-case err
+                         (tree-sitter-langs-compile lang-symbol clean)
+                       (error `[,lang-symbol ,err])))))))
     (message "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     (unwind-protect
         (let* ((tar-file (concat (file-name-as-directory
@@ -359,9 +352,11 @@ non-nil."
 (defun tree-sitter-langs--copy-query (lang-symbol &optional force)
   "Copy highlights.scm file of LANG-SYMBOL to `tree-sitter-langs--queries-dir'.
 This assumes the repo has already been set up, for example by
-`tree-sitter-langs-compile'."
+`tree-sitter-langs-compile'.
+
+If the optional arg FORCE is non-nil, any existing file will be overwritten."
   (let ((src (thread-first tree-sitter-langs--repos-dir
-               (concat (format "tree-sitter-%s" lang-symbol))
+               (concat (tree-sitter-langs--repo-name lang-symbol))
                file-name-as-directory (concat "queries")
                file-name-as-directory (concat "highlights.scm"))))
     (when (file-exists-p src)
@@ -381,8 +376,9 @@ This assumes the repo has already been set up, for example by
   "Copy highlights.scm files to `tree-sitter-langs--queries-dir'.
 This assumes the repos have already been cloned set up, for example by
 `tree-sitter-langs-create-bundle'."
-  (pcase-dolist (`(,lang-symbol . _) tree-sitter-langs-repos)
-    (tree-sitter-langs--copy-query lang-symbol :force)))
+  (tree-sitter-langs--map-repos
+   (lambda (name)
+     (tree-sitter-langs--copy-query (tree-sitter-langs--symbol name) :force))))
 
 (provide 'tree-sitter-langs-build)
 ;;; tree-sitter-langs-build.el ends here
