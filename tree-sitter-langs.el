a516144aa9b109582e89702e5d76217e8fae907b
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2021-07-24 17:54:51 +0700

    State the intent to include query patterns for other functionalities

--- tree-sitter-langs.el
+++ tree-sitter-langs.el
@@ -26,6 +26,12 @@
 ;;    that are not aware of `tree-sitter'. A language major mode that wants to
 ;;    use `tree-sitter' for syntax highlighting should instead provide the query
 ;;    patterns on its own, using the mechanisms defined by `tree-sitter-hl'.
+;;
+;; 3. Optional query patterns for other minor modes that provide high-level
+;;    functionalities on top of `tree-sitter', such as code folding, evil text
+;;    objects... As with highlighting patterns, major modes that are directly
+;;    aware of `tree-sitter' should provide the query patterns on their own.
+
 
 ;;; Code:
 
