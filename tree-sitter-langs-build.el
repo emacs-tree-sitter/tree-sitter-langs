2ac5686441a356b389739fbdbf61135b8f22137d
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2021-08-15 18:42:25 +0700

    Downgrade bundle version to 0.10.2 due to CI issues
    [skip ci]

--- tree-sitter-langs-build.el
+++ tree-sitter-langs-build.el
@@ -209,7 +209,7 @@ latest commit."
 ;; ---------------------------------------------------------------------------
 ;;; Building language grammars.
 
-(defconst tree-sitter-langs--bundle-version "0.10.3"
+(defconst tree-sitter-langs--bundle-version "0.10.2"
   "Version of the grammar bundle.
 This should be bumped whenever a language submodule is updated, which should be
 infrequent (grammar-only changes). It is different from the version of
