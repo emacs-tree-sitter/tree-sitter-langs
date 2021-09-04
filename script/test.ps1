35aa3f1f482f2a0af2fc1401fec36f7aecd311b9
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2021-09-04 13:16:03 +0700

    Allow specifying a language when running script/test

--- script/test.ps1
+++ script/test.ps1
@@ -1,9 +1,16 @@
 $here = $PSScriptRoot
 $project_root = (Get-Item $here).Parent.FullName
+$lang = $args[0]
+
+if ("all",$null -contains $lang) {
+    $selector = 'nil'
+} else {
+    $selector = "\`"queries/${lang}\`""
+}
 
 Push-Location $project_root
 cask emacs --batch `
   -l ert `
   -l tree-sitter-langs-tests.el `
-  -f ert-run-tests-batch-and-exit
+  --eval "(ert-run-tests-batch-and-exit ${selector})"
 Pop-Location
