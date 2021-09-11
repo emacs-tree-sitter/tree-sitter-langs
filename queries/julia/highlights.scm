4c6c73eb4408d78a4df72e4147babe4c573a1e75
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-09-11 01:58:18 +0100

    queries(julia): add basic highlights.scm

---
+++ queries/julia/highlights.scm
@@ -0,0 +1,63 @@
+[
+ ;; types
+ "struct"
+ "abstract"
+ "mutable"
+ "type"
+ "where"
+
+ ;; functions
+ "function"
+
+ ;; imports
+ "module"
+
+ ;; if
+ "if"
+
+ ;; loops
+ "for"
+ "while"
+ "do"
+
+ "break"
+ "continue"
+
+ ;; exceptions
+ "try"
+ "catch"
+ "finnally"
+
+ ;; blocks
+ "quote"
+ "let"
+ "begin"
+ "end"
+
+ ;vars
+ "local"
+ "global"
+ "const"
+ ] @keyword
+
+[
+ "="
+ "=="
+ "~="
+ "..."
+ ] @operators
+
+[
+ "("
+ ")"
+ "["
+ "]"
+ "{"
+ "}"
+ ] @punctuation.bracket
+
+[
+ ","
+ "."
+ ";"
+ ] @punctuation.delimters
