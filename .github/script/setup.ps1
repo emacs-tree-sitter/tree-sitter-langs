4d8d39837035c3db7fe061c45ad422000bf6af5f
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2021-09-17 16:08:58 +0700

    ci: Add helper scripts for GitHub Actions

---
+++ .github/script/setup.ps1
@@ -0,0 +1,6 @@
+iex (New-Object net.webclient).downloadstring('https://get.scoop.sh')
+Join-Path (Resolve-Path ~).Path "scoop\shims" >> $env:GITHUB_PATH
+scoop bucket add extras
+scoop bucket add nonportable
+scoop update
+scoop checkup
