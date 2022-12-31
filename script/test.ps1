4f7168237aaa61954a14b3bd2c3b8171587ecb01
Abin Simon <mail@meain.io>
2022-12-31 15:57:49 +0530

    Fix test CI

--- script/test.ps1
+++ script/test.ps1
@@ -10,7 +10,8 @@ if ("all",$null -contains $lang) {
 
 Push-Location $project_root
 try {
-    cask emacs --batch `
+    cask emacs -Q --batch `
+      --directory . `
       -l ert `
       -l tree-sitter-langs-tests.el `
       --eval "(ert-run-tests-batch-and-exit ${selector})"
