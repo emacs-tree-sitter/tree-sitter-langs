578fc9495f0539e259337c8fbc5cc85d4ace7517
Peter Zimmermann <pzimm30@gmail.com>
2023-01-21 11:38:24 -0500

    add fennel major mode mapping

--- tree-sitter-langs.el
+++ tree-sitter-langs.el
@@ -144,7 +144,8 @@ See `tree-sitter-langs-repos'."
                 (typescript-mode . typescript)
                 (verilog-mode    . verilog)
                 (yaml-mode       . yaml)
-                (zig-mode        . zig))))
+                (zig-mode        . zig)
+								(fennel-mode     . fennel))))
     (cl-pushnew entry tree-sitter-major-mode-language-alist
                 :key #'car))
   (advice-remove 'tree-sitter--setup #'tree-sitter-langs--init-major-mode-alist))
