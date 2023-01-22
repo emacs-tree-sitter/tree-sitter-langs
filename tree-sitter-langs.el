e54f820bd025b69ab6e32f1720130ec8fb1cdefb
Peter Zimmermann <pzimm30@gmail.com>
2023-01-22 13:09:22 -0500

    fix parenthesis

--- tree-sitter-langs.el
+++ tree-sitter-langs.el
@@ -145,7 +145,7 @@ See `tree-sitter-langs-repos'."
                 (typescript-mode . typescript)
                 (verilog-mode    . verilog)
                 (yaml-mode       . yaml)
-                (zig-mode        . zig)))
+                (zig-mode        . zig))))
     (cl-pushnew entry tree-sitter-major-mode-language-alist
                 :key #'car))
   (advice-remove 'tree-sitter--setup #'tree-sitter-langs--init-major-mode-alist))
