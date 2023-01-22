78dff9059750f11c4942d723ff0db2af8d3acfbb
Peter Zimmermann <pzimm30@gmail.com>
2023-01-22 00:01:44 -0500

    Fix formatting error

--- tree-sitter-langs.el
+++ tree-sitter-langs.el
@@ -145,7 +145,7 @@ See `tree-sitter-langs-repos'."
                 (verilog-mode    . verilog)
                 (yaml-mode       . yaml)
                 (zig-mode        . zig)
-								(fennel-mode     . fennel))))
+				(fennel-mode     . fennel))))
     (cl-pushnew entry tree-sitter-major-mode-language-alist
                 :key #'car))
   (advice-remove 'tree-sitter--setup #'tree-sitter-langs--init-major-mode-alist))
