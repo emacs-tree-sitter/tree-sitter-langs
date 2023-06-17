7c270e1a8c3daa8fb00cd4939fd1bf182a2c4c23
JenChieh <jcs090218@gmail.com>
2023-06-17 00:01:51 -0700

    ci: Build with Eask

--- script/bundle-version.ps1
+++ script/bundle-version.ps1
@@ -1,1 +1,1 @@
-cask eval "(progn (require 'tree-sitter-langs-build) (princ tree-sitter-langs--bundle-version))"
+eask eval "(progn (require 'tree-sitter-langs-build) (princ tree-sitter-langs--bundle-version))"
