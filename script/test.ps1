c47fd900158adebaa6e54f298c2434135328231a
JenChieh <jcs090218@gmail.com>
2023-06-17 00:01:51 -0700

    ci: Build with Eask

--- script/test.ps1
+++ script/test.ps1
@@ -10,7 +10,7 @@ if ("all",$null -contains $lang) {
 
 Push-Location $project_root
 try {
-    cask emacs -Q --batch `
+    eask emacs -Q --batch `
       --directory . `
       -l ert `
       -l tree-sitter-langs-tests.el `
