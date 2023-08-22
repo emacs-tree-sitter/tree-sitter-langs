8c0485b15955671f03a1233723968638ff933bf0
JenChieh <jcs090218@gmail.com>
2023-08-22 12:20:19 -0700

    comment invalid grammars

--- queries/jai/highlights.scm
+++ queries/jai/highlights.scm
@@ -13,14 +13,14 @@
 (number) @number
 (function_definition name: (identifier) @function)
 ;;(struct_decl name: (identifier) @type.user)
-(constant_value_definition name: (identifier) @constant)
+;;(constant_value_definition name: (identifier) @constant)
 (inline_comment) @comment
 (block_comment) @comment
 (cast_expression name: (identifier) @function)
-(function_call name: (variable_reference) @function)
-(variable_decl name: (identifier) @variable)
-(variable_decl names: (identifier) @variable)
-(implicit_variable_decl name: (identifier) @variable)
-(parameter_decl name: (identifier) @variable)
+;;(function_call name: (variable_reference) @function)
+;;(variable_decl name: (identifier) @variable)
+;;(variable_decl names: (identifier) @variable)
+;;(implicit_variable_decl name: (identifier) @variable)
+;;(parameter_decl name: (identifier) @variable)
 (for_loop name: (identifier) @variable)
 (for_loop names: (identifier) @variable)
