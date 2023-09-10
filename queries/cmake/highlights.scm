dd077ecab8fdc8bc808fbc1b94e75d0c37a4c28f
JenChieh <jcs090218@gmail.com>
2023-09-09 17:52:36 -0700

    fix hl

--- queries/cmake/highlights.scm
+++ queries/cmake/highlights.scm
@@ -89,39 +89,39 @@
 ;;   ) .
 ;; )
 
-((argument) @boolean
-  (#match? @boolean "\\c^(1|on|yes|true|y|0|off|no|false|n|ignore|notfound|.*-notfound)$")
-)
-
-(if_command
-  (if)
-  (argument) @keyword.operator
-  (#any-of? @keyword.operator "NOT" "AND" "OR"
-                              "COMMAND" "POLICY" "TARGET" "TEST" "DEFINED" "IN_LIST"
-                              "EXISTS" "IS_NEWER_THAN" "IS_DIRECTORY" "IS_SYMLINK" "IS_ABSOLUTE"
-                              "MATCHES"
-                              "LESS" "GREATER" "EQUAL" "LESS_EQUAL" "GREATER_EQUAL"
-                              "STRLESS" "STRGREATER" "STREQUAL" "STRLESS_EQUAL" "STRGREATER_EQUAL"
-                              "VERSION_LESS" "VERSION_GREATER" "VERSION_EQUAL" "VERSION_LESS_EQUAL" "VERSION_GREATER_EQUAL"
-  )
-)
-
-(normal_command
-  (identifier) @function.builtin
-  . (argument)
-  (argument) @constant
-  (#any-of? @constant "ALL" "COMMAND" "DEPENDS" "BYPRODUCTS" "WORKING_DIRECTORY" "COMMENT"
-                   "JOB_POOL" "VERBATIM" "USES_TERMINAL" "COMMAND_EXPAND_LISTS" "SOURCES")
-  (#match? @function.builtin "\\c^(add_custom_target)$")
-)
-
-(normal_command
-  (identifier) @function.builtin
-  (argument) @constant
-  (#any-of? @constant "OUTPUT" "COMMAND" "MAIN_DEPENDENCY" "DEPENDS" "BYPRODUCTS" "IMPLICIT_DEPENDS" "WORKING_DIRECTORY"
-                      "COMMENT" "DEPFILE" "JOB_POOL" "VERBATIM" "APPEND" "USES_TERMINAL" "COMMAND_EXPAND_LISTS")
-  (#match? @function.builtin "\\c^(add_custom_command)$")
-)
+;; ((argument) @boolean
+;;   (#match? @boolean "\\c^(1|on|yes|true|y|0|off|no|false|n|ignore|notfound|.*-notfound)$")
+;; )
+;;
+;; (if_command
+;;   (if)
+;;   (argument) @keyword.operator
+;;   (#any-of? @keyword.operator "NOT" "AND" "OR"
+;;                               "COMMAND" "POLICY" "TARGET" "TEST" "DEFINED" "IN_LIST"
+;;                               "EXISTS" "IS_NEWER_THAN" "IS_DIRECTORY" "IS_SYMLINK" "IS_ABSOLUTE"
+;;                               "MATCHES"
+;;                               "LESS" "GREATER" "EQUAL" "LESS_EQUAL" "GREATER_EQUAL"
+;;                               "STRLESS" "STRGREATER" "STREQUAL" "STRLESS_EQUAL" "STRGREATER_EQUAL"
+;;                               "VERSION_LESS" "VERSION_GREATER" "VERSION_EQUAL" "VERSION_LESS_EQUAL" "VERSION_GREATER_EQUAL"
+;;   )
+;; )
+;;
+;; (normal_command
+;;   (identifier) @function.builtin
+;;   . (argument)
+;;   (argument) @constant
+;;   (#any-of? @constant "ALL" "COMMAND" "DEPENDS" "BYPRODUCTS" "WORKING_DIRECTORY" "COMMENT"
+;;                    "JOB_POOL" "VERBATIM" "USES_TERMINAL" "COMMAND_EXPAND_LISTS" "SOURCES")
+;;   (#match? @function.builtin "\\c^(add_custom_target)$")
+;; )
+;;
+;; (normal_command
+;;   (identifier) @function.builtin
+;;   (argument) @constant
+;;   (#any-of? @constant "OUTPUT" "COMMAND" "MAIN_DEPENDENCY" "DEPENDS" "BYPRODUCTS" "IMPLICIT_DEPENDS" "WORKING_DIRECTORY"
+;;                       "COMMENT" "DEPFILE" "JOB_POOL" "VERBATIM" "APPEND" "USES_TERMINAL" "COMMAND_EXPAND_LISTS")
+;;   (#match? @function.builtin "\\c^(add_custom_command)$")
+;; )
 
 (escape_sequence) @string.escape
 
