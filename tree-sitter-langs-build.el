ccb5529406fe2a1f9e54d6425ebf4931cd4c59c7
Tuấn-Anh Nguyễn <ubolonton@users.noreply.github.com>
2024-04-22 21:33:15 +0000

    Release 0.12.180

--- tree-sitter-langs-build.el
+++ tree-sitter-langs-build.el
@@ -232,7 +232,7 @@ latest commit."
 ;; ---------------------------------------------------------------------------
 ;;; Building language grammars.
 
-(defconst tree-sitter-langs--bundle-version "0.12.179"
+(defconst tree-sitter-langs--bundle-version "0.12.180"
   "Version of the grammar bundle.
 This should be bumped whenever a language submodule is updated, which should be
 infrequent (grammar-only changes).  It is different from the version of
