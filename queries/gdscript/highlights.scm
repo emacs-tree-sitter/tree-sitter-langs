d8ad19e72b3fee29dea1107bf7c69809b50f04fe
Jen-Chieh Shen <jcs090218@gmail.com>
2025-09-28 00:44:15 -0700

    Remove the "tool" statement from Godot 3

--- queries/gdscript/highlights.scm
+++ queries/gdscript/highlights.scm
@@ -15,7 +15,6 @@
 (set_body "set" @keyword)
 (get_body "get" @keyword)
 (static_keyword) @type.qualifier
-(tool_statement) @keyword
 (breakpoint_statement) @debug
 (inferred_type) @operator
 [(true) (false)] @boolean
@@ -188,7 +187,7 @@
   "export_flags_3d_physics" "export_flags_3d_render" "export_global_dir"
   "export_global_file" "export_group" "export_multiline" "export_node_path"
   "export_placeholder" "export_range" "export_subgroup" "icon" "onready"
-  "rpc" "tool" "warning_ignore"
+  "rpc" "warning_ignore"
  ))
 
 ;; Builtin Types
