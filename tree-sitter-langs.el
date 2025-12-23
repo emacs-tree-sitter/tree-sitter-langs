275768141d7958c2ba2128084d83f1ec47e8dd14
Tuấn-Anh Nguyễn <ubolonton@users.noreply.github.com>
2025-12-23 05:20:50 +0000

    Release 0.12.336

--- tree-sitter-langs.el
+++ tree-sitter-langs.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Jen-Chieh Shen <jcs090218@gmail.com>
 ;; Keywords: languages tools parsers tree-sitter
 ;; Homepage: https://github.com/emacs-tree-sitter/tree-sitter-langs
-;; Version: 0.12.335
+;; Version: 0.12.336
 ;; Package-Requires: ((emacs "26.1") (tree-sitter "0.15.0"))
 ;; SPDX-License-Identifier: MIT
 
