96bb4da972fb59801b0ecf375f0c5bc161c387e0
Jen-Chieh Shen <jcs090218@gmail.com>
2025-12-23 18:28:30 -0800

    Update highlights.scm

--- queries/haxe/highlights.scm
+++ queries/haxe/highlights.scm
@@ -1,4 +1,4 @@
-(identifier !type) @variable
+(identifier) @variable
 (comment) @comment
 
 ; Preprocessor Statement
@@ -15,27 +15,14 @@
 ; --------------
 (type_params
   "<" @punctuation.bracket
-  (type_param (identifier) @type)
   ">" @punctuation.bracket
 )
 
 ; Declarations
 ; ------------
-(import_statement name: [
-  (identifier) @type 
-;   (_ (identifier) @type)
-;   (_(_ (identifier) @type))
-;   (_(_(_ (identifier) @type)))
-;   (_(_(_(_ (identifier) @type))))
-;   (_(_(_(_(_ (identifier) @type)))))
-])
-(package_statement name: (identifier) @type) 
 
 (class_declaration name: (identifier) @type.definition)
-(class_declaration super_class_name: (identifier) @type.definition)
-(class_declaration interface_name: (identifier) @type.definition)
 (interface_declaration name: (identifier) @type.definition)
-(interface_declaration interface_name: (identifier) @type.definition)
 (typedef_declaration name: (identifier) @type.definition)
 
 (function_declaration name: (identifier) @function)
@@ -48,34 +35,82 @@
 ; TODO: Figure out how to determined when "nested member call" is last ident.
 ; apparently this is a known issue https://github.com/tree-sitter/tree-sitter/issues/880
 (call_expression object: [
-  (identifier) @function 
-  (_ (identifier) @method .)
-  (_(_ (identifier) @method .))
-  (_(_(_ (identifier) @method .)))
-  (_(_(_(_ (identifier) @method .))))
-  (_(_(_(_(_ (identifier) @method .)))))
+  (_) @function
+  (_ (identifier) @function .)
+;   (_(_ (identifier) @function .))
+;   (_(_(_ (identifier) @function .)))
+;   (_(_(_(_ (identifier) @function .))))
+;   (_(_(_(_(_ (identifier) @function .)))))
 ])
 
 ; Literals
 ; --------
-[(keyword) (null)] @keyword
+; [(keyword) (null)] @keyword
 ; (type) @type
+(type_name) @type
+(package_name) @module
 (type (identifier) !built_in) @type
 (type built_in: (identifier)) @type.builtin
 [(integer) (float)] @number
 (string) @string
-(bool) @constant
+(bool) @boolean
 (operator) @operator
 (escape_sequence) @punctuation
+(null) @constant.builtin
+(access_identifiers "null" @keyword)
 
-; Interpolation
-; -------------
-(interpolation "$" @punctuation.special)
-(interpolation
-  "${" @punctuation.special
-  "}" @punctuation.special
-) @embedded
+; Keywords
+; --------
+[
+  "abstract"
+  "as"
+  "break"
+  "case"
+  "cast"
+  "catch"
+  "class"
+  "continue"
+  "default"
+  "do"
+  "dynamic"
+  "else"
+  "enum"
+  "extends"
+  "extern"
+  "final"
+  "for"
+  "function"
+  "if"
+  "implements"
+  "import"
+  "in"
+  "inline"
+  "interface"
+  "macro"
+  "operator"
+  "overload"
+  "override"
+  "package"
+  "private"
+  "public"
+  "return"
+  "static"
+  "switch"
+  "this"
+  "throw"
+  "try"
+  "typedef"
+  "untyped"
+  "using"
+  "var"
+  "while"
+] @keyword
 
+(function_declaration name: "new" @constructor)
+(call_expression
+  "new" @keyword
+  constructor: (type_name) @constructor
+)
 
 ; Tokens
 ; ------
@@ -98,3 +133,12 @@
 ;   "."
   ","
 ] @punctuation.delimiter
+
+
+; Interpolation
+; -------------
+(interpolation "$" @punctuation.special)
+(interpolation
+  "${" @punctuation.special
+  "}" @punctuation.special
+) @embedded
