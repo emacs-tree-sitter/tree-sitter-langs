275682d5a6269d787af975b67e2884a9447b7956
JenChieh <jcs090218@gmail.com>
2026-01-03 22:02:38 +0800

    feat: Add git-config

---
+++ queries/git-config/highlights.scm
@@ -0,0 +1,28 @@
+(section_name) @tag
+
+((section_name) @function.builtin
+ (#eq? @function.builtin "include"))
+
+((section_header
+   (section_name) @function.builtin
+   (subsection_name))
+ (#eq? @function.builtin "includeIf"))
+
+(variable (name) @property)
+[(true) (false)] @constant.builtin
+(integer) @number
+
+[(string) (subsection_name)] @string
+
+((string) @string.special.path
+ (#match? @string.special.path "^(~|./|/)"))
+
+[
+  "["
+  "]"
+  "\""
+] @punctuation.bracket
+
+"=" @punctuation.delimiter
+
+(comment) @comment
