248c621770ff1fe20566ec948c389fbf2884458a
JenChieh <jcs090218@gmail.com>
2026-01-03 22:07:03 +0800

    fix: lint

--- queries/git-config/highlights.scm
+++ queries/git-config/highlights.scm
@@ -1,21 +1,30 @@
 (section_name) @tag
 
 ((section_name) @function.builtin
- (#eq? @function.builtin "include"))
+  (#eq? @function.builtin "include"))
 
 ((section_header
-   (section_name) @function.builtin
-   (subsection_name))
- (#eq? @function.builtin "includeIf"))
+  (section_name) @function.builtin
+  (subsection_name))
+  (#eq? @function.builtin "includeIf"))
 
-(variable (name) @property)
-[(true) (false)] @constant.builtin
+(variable
+  (name) @property)
+
+[
+  (true)
+  (false)
+] @constant.builtin
+
 (integer) @number
 
-[(string) (subsection_name)] @string
+[
+  (string)
+  (subsection_name)
+] @string
 
 ((string) @string.special.path
- (#match? @string.special.path "^(~|./|/)"))
+  (#match? @string.special.path "^(~|./|/)"))
 
 [
   "["
