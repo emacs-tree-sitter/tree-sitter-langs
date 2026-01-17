d0d086f7591a4a5e314336fc7658933c0c43dec0
JenChieh <jcs090218@gmail.com>
2026-01-17 20:06:08 +0800

    fix: queries

--- queries/crystal/highlights.scm
+++ queries/crystal/highlights.scm
@@ -1,284 +1,282 @@
-                                        ; Early rules
-                                        ; These patterns may be overridden later
+; Early rules
+; These patterns may be overridden later
+[
+  ","
+  ";"
+  "."
+  ":"
+] @punctuation.delimiter
+
+; Keywords
+[
+  "alias"
+  "annotation"
+  "asm"
+  "begin"
+  "break"
+  "case"
+  "do"
+  "end"
+  "ensure"
+  "extend"
+  "in"
+  "include"
+  "next"
+  "of"
+  "select"
+  "then"
+  "verbatim"
+  "when"
+] @keyword
 
 [
- ","
- ";"
- "."
- ":"
- ] @punctuation.delimiter
-
-                                        ; Keywords
+  "def"
+  "fun"
+  "macro"
+] @keyword.function
 
 [
- "alias"
- "annotation"
- "asm"
- "begin"
- "break"
- "case"
- "do"
- "end"
- "ensure"
- "extend"
- "in"
- "include"
- "next"
- "of"
- "select"
- "then"
- "verbatim"
- "when"
- ] @keyword
-
-[
- "def"
- "fun"
- "macro"
- ] @keyword.function
-
-[
- "class"
- "enum"
- "lib"
- "module"
- "struct"
- "type"
- "union"
- ] @keyword.type
+  "class"
+  "enum"
+  "lib"
+  "module"
+  "struct"
+  "type"
+  "union"
+] @keyword.type
 
 "require" @keyword.import
 
 [
- "return"
- "yield"
- ] @keyword.return
+  "return"
+  "yield"
+] @keyword.return
 
 [
- "if"
- "else"
- "elsif"
- "unless"
- ] @keyword.conditional
+  "if"
+  "else"
+  "elsif"
+  "unless"
+] @keyword.conditional
 
 (conditional
- [
-  "?"
-  ":"
+  [
+    "?"
+    ":"
   ] @keyword.conditional.ternary)
 
 [
- "for"
- "until"
- "while"
- ] @keyword.repeat
+  "for"
+  "until"
+  "while"
+] @keyword.repeat
 
 "rescue" @keyword.exception
 
 [
- (private)
- (protected)
- "abstract"
- ] @keyword.modifier
+  (private)
+  (protected)
+  "abstract"
+] @keyword.modifier
 
 (pseudo_constant) @constant.builtin
 
-                                        ; literals
+; literals
 (string
- "\"" @string)
+  "\"" @string)
 
 (string
- (literal_content) @string)
+  (literal_content) @string)
 
 (string
- (escape_sequence) @string.escape)
+  (escape_sequence) @string.escape)
 
 (symbol
- [
-  ":"
-  ":\""
-  "\""
+  [
+    ":"
+    ":\""
+    "\""
   ] @string.special.symbol)
 
 (symbol
- (literal_content) @string.special.symbol)
+  (literal_content) @string.special.symbol)
 
 (symbol
- (escape_sequence) @character)
+  (escape_sequence) @character)
 
 (command
- "`" @string.special)
+  "`" @string.special)
 
 (command
- (literal_content) @string.special)
+  (literal_content) @string.special)
 
 (command
- (escape_sequence) @character)
+  (escape_sequence) @character)
 
 (regex
- "/" @punctuation.bracket)
+  "/" @punctuation.bracket)
 
 (regex
- (literal_content) @string.regexp)
+  (literal_content) @string.regexp)
 
 (regex_modifier) @character.special
 
 (heredoc_body
- (literal_content) @string)
+  (literal_content) @string)
 
 (heredoc_body
- (escape_sequence) @string.escape)
+  (escape_sequence) @string.escape)
 
 [
- (heredoc_start)
- (heredoc_end)
- ] @label
+  (heredoc_start)
+  (heredoc_end)
+] @label
 
 (char
- "'" @character)
+  "'" @character)
 
 (char
- (literal_content) @character)
+  (literal_content) @character)
 
 (char
- (escape_sequence) @string.escape)
+  (escape_sequence) @string.escape)
 
 (integer) @number
 
 (float) @number.float
 
 [
- (true)
- (false)
- ] @boolean
+  (true)
+  (false)
+] @boolean
 
 (nil) @constant.builtin
 
 ((comment) @comment
-                                        ; Set priority so macro expressions in comments are not grayed out
- (#set! priority 95))
+  ; Set priority so macro expressions in comments are not grayed out
+  (#set! priority 95))
 
-                                        ; Operators and punctuation
+; Operators and punctuation
 [
- "="
- "=>"
- "->"
- "&"
- "*"
- "**"
- (operator)
- ] @operator
+  "="
+  "=>"
+  "->"
+  "&"
+  "*"
+  "**"
+  (operator)
+] @operator
 
 [
- "("
- ")"
- "["
- "@["
- "]"
- "{"
- "}"
- ] @punctuation.bracket
+  "("
+  ")"
+  "["
+  "@["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
 
 ([
   "{{"
   "}}"
-  ] @punctuation.bracket
-                                        ; Set priority so "a{{b}}" is highlighted as brackets, not string content
-                                        ;                   ^^
-    (#set! priority 105))
+] @punctuation.bracket
+  ; Set priority so "a{{b}}" is highlighted as brackets, not string content
+  ;                   ^^
+  (#set! priority 105))
 
 (index_call
- method: (operator) @punctuation.bracket
- [
-  "]"
-  "]?"
+  method: (operator) @punctuation.bracket
+  [
+    "]"
+    "]?"
   ] @punctuation.bracket)
 
 (block
- "|" @punctuation.bracket)
+  "|" @punctuation.bracket)
 
 [
- "{%"
- "%}"
- ] @tag.delimiter
+  "{%"
+  "%}"
+] @tag.delimiter
 
 (interpolation
- "#{" @punctuation.special
- "}" @punctuation.special)
+  "#{" @punctuation.special
+  "}" @punctuation.special)
 
-                                        ; Types
+; Types
 [
- (constant)
- (generic_instance_type)
- (generic_type)
- ] @type
+  (constant)
+  (generic_instance_type)
+  (generic_type)
+] @type
 
 (nilable_constant
- "?" @type.builtin)
+  "?" @type.builtin)
 
 (nilable_type
- "?" @type.builtin)
+  "?" @type.builtin)
 
 (union_type
- "|" @operator)
+  "|" @operator)
 
 (annotation
- (constant) @attribute)
+  (constant) @attribute)
 
 (method_def
- name: (identifier) @function.method)
+  name: (identifier) @function.method)
 
 (macro_def
- name: (identifier) @function.method)
+  name: (identifier) @function.method)
 
 (abstract_method_def
- name: (identifier) @function.method)
+  name: (identifier) @function.method)
 
 (fun_def
- name: (identifier) @function
- real_name: (identifier)? @function)
+  name: (identifier) @function
+  real_name: (identifier)? @function)
 
 (param
- name: (identifier) @variable.parameter)
+  name: (identifier) @variable.parameter)
 
 (splat_param
- name: (identifier) @variable.parameter)
+  name: (identifier) @variable.parameter)
 
 (double_splat_param
- name: (identifier) @variable.parameter)
+  name: (identifier) @variable.parameter)
 
 (block_param
- name: (identifier) @variable.parameter)
+  name: (identifier) @variable.parameter)
 
 (fun_param
- name: (identifier) @variable.parameter)
+  name: (identifier) @variable.parameter)
 
 (rescue
- variable: (identifier) @variable.parameter)
+  variable: (identifier) @variable.parameter)
 
 (macro_var
- name: (identifier) @variable)
+  name: (identifier) @variable)
 
 [
- (class_var)
- (instance_var)
- ] @variable.member
+  (class_var)
+  (instance_var)
+] @variable.member
 
 (underscore) @variable.parameter.builtin
 
 (self) @variable.builtin
 
 (named_tuple
- (named_expr
-  name: (identifier) @property))
+  (named_expr
+    name: (identifier) @property))
 
 (argument_list
- (named_expr
-  name: (identifier) @property))
+  (named_expr
+    name: (identifier) @property))
 
 (named_type
- name: (identifier) @property)
+  name: (identifier) @property)
 
-                                        ; function calls
+; function calls
 (call
- method: (identifier) @function.call)
+  method: (identifier) @function.call)
